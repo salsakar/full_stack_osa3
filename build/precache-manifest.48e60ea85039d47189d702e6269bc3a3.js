c9701e3d56afe154802d853c0e5fdb401b0ac419
Sakari Salla <sakari.salla@gmail.com>
2019-09-30 09:48:20 +0300

    fsa

---
+++ build/precache-manifest.48e60ea85039d47189d702e6269bc3a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34375bb91cbd56175472c69df036c98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9652bff24cf167e11db",
+    "url": "/static/css/main.0c5275d1.chunk.css"
+  },
+  {
+    "revision": "d669a1880791d7e757be",
+    "url": "/static/js/2.d390b39c.chunk.js"
+  },
+  {
+    "revision": "f9652bff24cf167e11db",
+    "url": "/static/js/main.d5a363fe.chunk.js"
+  },
+  {
+    "revision": "a4e9cb56398c85fd6c45",
+    "url": "/static/js/runtime-main.33bda74f.js"
+  }
+]);
