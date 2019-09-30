68665295bd7cbd1cb306d836e2b1a4fd6790e40b
Sakari Salla <sakari.salla@gmail.com>
2019-09-30 09:23:55 +0300

    joo

---
+++ build/precache-manifest.8d86df2056dc388a111875e3770df03b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f005603b3d7e6c579d6cee9106f37e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efda127b843f32ff52e1",
+    "url": "/static/css/main.0c5275d1.chunk.css"
+  },
+  {
+    "revision": "d669a1880791d7e757be",
+    "url": "/static/js/2.d390b39c.chunk.js"
+  },
+  {
+    "revision": "efda127b843f32ff52e1",
+    "url": "/static/js/main.4b2d5db4.chunk.js"
+  },
+  {
+    "revision": "a4e9cb56398c85fd6c45",
+    "url": "/static/js/runtime-main.33bda74f.js"
+  }
+]);
