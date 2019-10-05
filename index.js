cdf296e2bed2424e38037e484a6e2b1a3ff4b83b
Sakari Salla <sakari.salla@gmail.com>
2019-10-05 06:34:39 +0300

    prod

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const personSchema = new mongoose.Schema({
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
-    
+
     delete returnedObject._id
     delete returnedObject.__v
   }
@@ -45,11 +45,12 @@ personSchema.set('toJSON', {
 morgan.token('body', (request) => {
   return (
     JSON.stringify(request.body)
+    
   )
 }
 )
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-/*app.use(morgan("tiny")) 
+/*app.use(morgan("tiny"))
 
 
 let persons = [
@@ -76,7 +77,7 @@ let persons = [
 ]*/
 
 function addZero(n) {
-  return (n < 10 ? '0' : '') + n;
+  return (n < 10 ? '0' : '') + n
 }
 
 const d = new Date()
@@ -108,7 +109,7 @@ app.get('/info', (request, response) => {
   }
 })*/
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     if (person) {
       response.json(person.toJSON())
@@ -169,9 +170,9 @@ app.post('/api/persons', (request, response, next) => {
   //persons = persons.concat(person)
   //response.json(person)
   person.save()
-      .then(savedPerson => {
-        response.json(savedPerson.toJSON())
-  })
+    .then(savedPerson => {
+      response.json(savedPerson.toJSON())
+    })
     .catch(error => next(error))
 })
 
@@ -197,7 +198,7 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-var uniqueValidator = require('mongoose-unique-validator');
+var uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -11,17 +11,16 @@ mongoose.connect(url, { useNewUrlParser: true })
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
-    name: { type: String, minlength: 3, required: true, unique: true },
-    number: { type: String, minlength: 8, required: true }
+const personSchema = new mongoose.Schema({
+  name: { type: String, minlength: 3, required: true, unique: true },
+  number: { type: String, minlength: 8, required: true }
 })
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 const Person = mongoose.model('persons', personSchema)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
-    
     delete returnedObject._id
     delete returnedObject.__v
   }
